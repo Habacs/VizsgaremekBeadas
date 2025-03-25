c24d3af0e6e193cc514635ddb8ca73392285756e
Rendszergazda <rendszergazda@blathy.info>
2025-03-25 09:26:50 +0100

    Navbar and routerView imported

---
+++ Frontend/ViewWebpage/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ 
+<Navbar/>
+<RouterView/>
+    
+</template>
+  
+<script>
+import {RouterView} from 'vue-router'
+import Navbar from './components/layout/Navbar.vue'
+export default{
+  components:{
+    RouterView,
+    Navbar
+  }
+}
+</script>
+  
