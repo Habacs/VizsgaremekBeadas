8362113ddbfddde52577ac585a98f1114d311019
Rendszergazda <rendszergazda@blathy.info>
2025-04-03 13:19:04 +0200

    cartButton created and initialized in App.vue

--- Frontend/ViewWebpage/src/App.vue
+++ Frontend/ViewWebpage/src/App.vue
@@ -2,16 +2,18 @@
  
 <Navbar/>
 <RouterView/>
-    
+<CartButton/>
 </template>
   
 <script>
 import {RouterView} from 'vue-router'
 import Navbar from './components/layout/Navbar.vue'
+import CartButton from './components/CartButton.vue';
 export default{
   components:{
     RouterView,
-    Navbar
+    Navbar,
+    CartButton
   }
 }
 </script>
