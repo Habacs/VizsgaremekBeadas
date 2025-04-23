7a3a121a486f89890e96c1bcea8216d9d8a8fc36
Szőke Simon <szoke.simon@gmail.com>
2025-04-23 12:46:08 +0200

    CartButton deleted

--- Frontend/ViewWebpage/src/App.vue
+++ Frontend/ViewWebpage/src/App.vue
@@ -2,18 +2,15 @@
  
 <Navbar/>
 <RouterView/>
-<CartButton/>
 </template>
   
 <script>
 import {RouterView} from 'vue-router'
 import Navbar from './components/layout/Navbar.vue'
-import CartButton from './components/CartButton.vue';
 export default{
   components:{
     RouterView,
     Navbar,
-    CartButton
   }
 }
 </script>
